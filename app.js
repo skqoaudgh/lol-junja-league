6bc7cac8c027b78d2836d92cf4a725a3c324cc00
myungho <skqoaudgh2@hanmail.net>
2020-07-11 14:17:21 +0900

    기록 API 및 라우팅 추가

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const compression = require('compression');
 
 const dotenv = require('dotenv').config();
 
+const Player = require('./model/Player');
+const Game = require('./model/Game');
+
 const app = express();
 
 mongoose.connect(process.env.MONGO_URL, {
@@ -25,6 +28,42 @@ app.use(favicon(path.join(__dirname, 'public/assets/favicon.ico')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res, next) => {
-  res.render('index.ejs');
+app.get('/', async (req, res, next) => {
+  const players = await Player.find().sort('name');
+  const games = await Game.find();
+  res.render('index.ejs', { players, games });
+});
+
+app.post('/', async (req, res, next) => {
+  try {
+    const { home, homeScore, away, awayScore } = req.body;
+    const winner = awayScore > homeScore ? away : home;
+    const loser = awayScore > homeScore ? home : away;
+
+    // Win Player
+    await Player.findOneAndUpdate(
+      { name: winner },
+      { $inc: { win: 1, score: 2 } }
+    );
+
+    // Lose Player
+    await Player.findOneAndUpdate(
+      { name: loser },
+      { $inc: { lose: 1, score: 1 } }
+    );
+
+    // Game
+    const game = new Game({
+      home,
+      away,
+      homeScore,
+      awayScore,
+      winner,
+    });
+    await game.save();
+
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+  }
 });
