c54fa93992e722099963dff2760a7b4bff65c6f8
myungho <skqoaudgh2@hanmail.net>
2020-07-11 13:52:05 +0900

    기본 설정 추가. 파비콘 추가. 압축 추가

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const favicon = require('serve-favicon');
+const path = require('path');
+const compression = require('compression');
+
 const dotenv = require('dotenv').config();
 
 const app = express();
@@ -10,3 +14,17 @@ mongoose.connect(process.env.MONGO_URL, {
 });
 
 app.listen(2000, console.log('Express server is opened on port 2000.'));
+
+app.engine('html', require('ejs').renderFile);
+
+app.use(compression());
+
+app.use(express.static('public'));
+app.use(favicon(path.join(__dirname, 'public/assets/favicon.ico')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res, next) => {
+  res.render('index.ejs');
+});
